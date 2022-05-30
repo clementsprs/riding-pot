2b0408d3ad7600c1d4cacf8c071c0632ab7e1f1d
clement spiers <clement.spiers@gmail.com>
2022-05-30 15:51:37 +0200

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating the users..."
+
+user1 = User.create([{ first_name: 'Clement' }, { last_name: 'Spiers' }, { username: 'Contadordu50' }, { username: 'Contadordu50' }, { email: 'clement@gmail.com' }, { password: 'clement@' }, { avatar: '' } ])
+User.create(name: 'DHH')
+
+puts "Seeding done."
