e42fc88ce151ff0de414f15d234317b8dcc718a0
clement spiers <clement.spiers@gmail.com>
2022-06-01 17:47:01 +0200

    create static images

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'fast_polylines'
 
 
 class RidesController < ApplicationController
@@ -27,5 +28,35 @@ class RidesController < ApplicationController
         lng: ride.longitude
       }
     end
+    @rides.each do |ride|
+    doc = Nokogiri::XML(ride.gpx_file)
+    trackpoints = doc.xpath('//xmlns:trkpt')
+    @markers = Array.new
+    trackpoints.each do |trkpt|
+      @markers << [
+        trkpt.xpath('@lon').to_s.to_f, trkpt.xpath('@lat').to_s.to_f
+      ]
+    end
+  end
+  @rides.each do |ride|
+    doc = Nokogiri::XML(ride.gpx_file)
+    trackpoints = doc.xpath('//xmlns:trkpt')
+    @markers = Array.new
+    trackpoints.each do |trkpt|
+      @markers << [
+        trkpt.xpath('@lat').to_s.to_f, trkpt.xpath('@lon').to_s.to_f
+      ]
+    end
+  end
+  newCoords = []
+  maxVal = 97
+  delta = (@markers.length / maxVal ).floor
+  i = 0
+  while i < @markers.length
+    newCoords.push(@markers[i])
+    i += delta
+  end
+  @path = FastPolylines.encode(newCoords)
+
   end
 end
