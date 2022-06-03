32b2085614a8b6c1c4938855b5eb96eb418e5f52
Margaux Pichard <margauxpichard@gmail.com>
2022-06-03 16:13:07 +0200

    fixed bugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def ride_status_text(status)
+    if status == "past_rides"
+      "Mes sorties précédentes"
+    else
+      "Mes sorties à venir"
+    end
+  end
+
+  def ride_status_css(status)
+    if status == "past_rides"
+      "0.2"
+    elsif status == "upcoming_rides"
+      "0"
+    end
+  end
 end
