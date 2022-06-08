259bfd17102cdd3afd569d831a72beb8ea1c698e
Margaux Pichard <margauxpichard@gmail.com>
2022-06-08 15:20:05 +0200

    commenting status

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -42,13 +42,12 @@ class Ride < ApplicationRecord
     return results.first.city
   end
 
-  def status!
-    raise
-    date_with_time = DateTime.new(date.year, date.month, date.day, starting_time.hour - 2, starting_time.min)
-    if date_with_time < DateTime.current
-      status = "done"
-    else
-      status = "upcoming"
-    end
-  end
+  # def status!
+  #   date_with_time = DateTime.new(date.year, date.month, date.day, starting_time.hour - 2, starting_time.min)
+  #   if date_with_time < DateTime.current
+  #     status = "done"
+  #   else
+  #     status = "upcoming"
+  #   end
+  # end
 end
